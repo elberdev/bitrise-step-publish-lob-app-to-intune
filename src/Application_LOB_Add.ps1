8aedcfebfc346aa188926613ecfe03b8cf001743
Ben Boral <ben.boral@bitrise.io>
2022-11-07 16:51:06 -0600

    exit correctly

--- src/Application_LOB_Add.ps1
+++ src/Application_LOB_Add.ps1
@@ -494,61 +494,6 @@ function GetAppCommitBody($contentVersionId, $LobType){
 
 ####################################################
 
-Function Get-MSIFileInformation(){
-
-# https://www.scconfigmgr.com/2014/08/22/how-to-get-msi-file-information-with-powershell/
-
-param(
-    [parameter(Mandatory=$true)]
-    [ValidateNotNullOrEmpty()]
-    [System.IO.FileInfo]$Path,
- 
-    [parameter(Mandatory=$true)]
-    [ValidateNotNullOrEmpty()]
-    [ValidateSet("ProductCode", "ProductVersion", "ProductName", "Manufacturer", "ProductLanguage", "FullVersion")]
-    [string]$Property
-)
-Process {
-
-    try {
-        # Read property from MSI database
-        $WindowsInstaller = New-Object -ComObject WindowsInstaller.Installer
-        $MSIDatabase = $WindowsInstaller.GetType().InvokeMember("OpenDatabase", "InvokeMethod", $null, $WindowsInstaller, @($Path.FullName, 0))
-        $Query = "SELECT Value FROM Property WHERE Property = '$($Property)'"
-        $View = $MSIDatabase.GetType().InvokeMember("OpenView", "InvokeMethod", $null, $MSIDatabase, ($Query))
-        $View.GetType().InvokeMember("Execute", "InvokeMethod", $null, $View, $null)
-        $Record = $View.GetType().InvokeMember("Fetch", "InvokeMethod", $null, $View, $null)
-        $Value = $Record.GetType().InvokeMember("StringData", "GetProperty", $null, $Record, 1)
- 
-        # Commit database and close view
-        $MSIDatabase.GetType().InvokeMember("Commit", "InvokeMethod", $null, $MSIDatabase, $null)
-        $View.GetType().InvokeMember("Close", "InvokeMethod", $null, $View, $null)           
-        $MSIDatabase = $null
-        $View = $null
- 
-        # Return the value
-        return $Value
-    }
-
-    catch {
-
-        Write-Warning -Message $_.Exception.Message;
-        break;
-    
-    }
-
-}
-
-    End {
-        # Run garbage collection and release ComObject
-        [System.Runtime.Interopservices.Marshal]::ReleaseComObject($WindowsInstaller) | Out-Null
-        [System.GC]::Collect()
-    }
-
-}
-
-####################################################
-
 Function Test-SourceFile(){
 
 param
@@ -573,7 +518,7 @@ param
 
 		Write-Host -ForegroundColor Red $_.Exception.Message;
         Write-Host
-		break;
+		throw;
 
     }
 
@@ -845,6 +790,7 @@ param
 	{
 		Write-Host "";
 		Write-Host -ForegroundColor Red "Aborting with exception: $($_.Exception.ToString())";
+		throw;
 	}
 }
 
@@ -1016,6 +962,7 @@ param
 	{
 		Write-Host "";
 		Write-Host -ForegroundColor Red "Aborting with exception: $($_.Exception.ToString())";
+		throw;
 	}
 }
 
@@ -1050,10 +997,10 @@ $sleep = 30
 ####################################################
 
 try {
-	# if($Env:android_apk_path){
-	# 	Write-Host "Uploading APK at path $Env:android_apk_path"
-	# 	Upload-AndroidLob -sourceFile $Env:android_apk_path -publisher $Env:android_publisher -description $Env:android_description -identityName $Env:android_identity_name -identityVersion $Env:android_identity_version -versionName $Env:android_version_name
-	# }
+	if($Env:android_apk_path){
+		Write-Host "Uploading APK at path $Env:android_apk_path"
+		Upload-AndroidLob -sourceFile $Env:android_apk_path -publisher $Env:android_publisher -description $Env:android_description -identityName $Env:android_identity_name -identityVersion $Env:android_identity_version -versionName $Env:android_version_name
+	}
 
 	if($Env:ios_ipa_path){
 		Write-Host "Uploading IPA at path $Env:ios_ipa_path"
