0b90a50c4234da1faaa1e8841ba10b2f0164f973
Ben Boral <82456480+benbitrise@users.noreply.github.com>
2022-11-11 10:37:52 -0600

    Update msft_auth.js

--- src/msft_auth.js
+++ src/msft_auth.js
@@ -6,10 +6,9 @@ const { ClientSecretCredential } = require("@azure/identity");
 const tenantId = process.env.MICROSOFT_TENANT_ID;
 const clientId = process.env.MICROSOFT_CLIENT_ID;
 const clientSecret = process.env.MICROSOFT_CLIENT_SECRET;
-const scopes = process.env.MICROSOFT_SCOPES;
 
 const credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
-const authProvider = new TokenCredentialAuthenticationProvider(credential, { scopes: [scopes] })
+const authProvider = new TokenCredentialAuthenticationProvider(credential, { scopes: ["https://graph.microsoft.com/.default"] })
 const client = Client.initWithMiddleware({
 	debugLogging: false,
 	authProvider,
